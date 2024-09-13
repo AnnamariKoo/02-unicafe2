cc9e47e3b1d0a430093f727190236590bee0610f
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-09-13 10:12:43 +0300

    Kolme painiketta lisätty.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import { useState } from "react";
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const handleGoodClick = () => {
+    setAll(allClicks.concat("G"));
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat("N"));
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setAll(allClicks.concat("B"));
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+    </div>
+  );
 };
 
 export default App;
